381a21809c317e47a36915e96e20d9181976798f
ThomasStanni <office@ts-visuals.at>
2026-04-10 19:45:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+async function loadNews() {
+  const res = await fetch('/.netlify/functions/news');
+  const data = await res.json();
 
+  const container = document.getElementById('news');
+
+  container.innerHTML = '';
+
+  data.forEach(article => {
+    const div = document.createElement('div');
+    div.innerText = article.title;
+    container.appendChild(div);
+  });
+}
+
+loadNews();
